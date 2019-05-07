2dc2637bb60e9cf4db09ebaf22ef494a726bd0c4
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-05-07 19:18:26 -0400

    settings

--- gatsby-node.js
+++ gatsby-node.js
@@ -61,7 +61,7 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
         ).then(result => {
           if (result.errors) {
             console.log(result.errors);
-            reject(result.errors);
+            return reject(result.errors);
           }
 
           const entries = result.data.allStoryblokEntry.edges;
